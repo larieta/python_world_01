fecedd83f8ab0414a3d1cce5696f5b835e2cc424
Lara Vieira <140765927+larieta@users.noreply.github.com>
2023-08-09 22:05:04 -0300

    introduction

---
+++ challenge_03.py
@@ -0,0 +1,5 @@
+print('Desafio 03!!')
+a = input ('digite um número: ')
+b = input('digite outro: ')
+print('o resultado da soma é: ', int(a) + int(b))
+
---
+++ test_01.py
@@ -0,0 +1,4 @@
+nome = input('qual é seu nome?')
+idade = input('e a sua idade?')
+altura = input('por último, qual a sua altura?')
+print(nome, idade, altura)
