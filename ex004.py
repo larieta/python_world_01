3f3ef6ea5e7239ae115c78d136863940424b6909
Lara Vieira <140765927+larieta@users.noreply.github.com>
2023-08-14 22:06:50 -0300

    dissecting a variable

--- ex004.py
+++ ex004.py
@@ -1,7 +1,7 @@
 n = (input('type something: '))
 
-print(f'''Primitive type? {type(n)} 
-Is it only made by spaces? {n.isspace() }
+print(f'''Primitive type: {type(n)} 
+Is it only made by spaces? {n.isspace()}
 Is it numeric? {n.isnumeric()} 
 Is it alphabetic? {n.isalpha()} 
 Is it alphanumeric? {n.isalnum()} 
